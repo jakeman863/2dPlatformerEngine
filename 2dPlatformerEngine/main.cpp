fb14c62af7bbc008a66edb7201e67f4ac14de1cb
jakeman863 <jaketheman01@gmail.com>
2019-02-03 18:50:08 -0600

    Test Window
    - Testing out SDL Window and Renderer

--- 2dPlatformerEngine/main.cpp
+++ 2dPlatformerEngine/main.cpp
@@ -3,10 +3,24 @@
 
 int main(int argc, char *argv[])
 {
-	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
-	{
-		std::cout << "SDL could not initialize! SDL Error: " << SDL_GetError() << std::endl;
-	}
 
-	return EXIT_SUCCESS;
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	// Flags to change window properties (Adding more flags requires | between each flag)
+	Uint32 flags = SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN;
+
+	// Creates a window (640 x 480) with the title "Game Window"
+	SDL_Window *window = SDL_CreateWindow("Game Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, flags);
+	// Simple rendering for testing
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+
+	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+
+	SDL_RenderClear(renderer);
+
+	SDL_RenderPresent(renderer);
+
+	SDL_Delay(3000);
+
+	return 0;
 }
