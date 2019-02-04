c31f1e8dd7567e0fff755d07dceb585313f6cc8e
jakeman863 <jaketheman01@gmail.com>
2019-02-03 18:14:39 -0600

    SDL Setup
    - Linked the SDL libraries to get started

---
+++ 2dPlatformerEngine/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <SDL.h>
+
+int main(int argc, char *argv[])
+{
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+	{
+		std::cout << "SDL could not initialize! SDL Error: " << SDL_GetError() << std::endl;
+	}
+
+	return EXIT_SUCCESS;
+}
