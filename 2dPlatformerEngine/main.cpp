222762a906de0779f858cc7635f6c9e970f78c0b
jakeman863 <jaketheman01@gmail.com>
2019-02-09 19:45:39 -0600

    Box2D
    - Box2D linked

--- 2dPlatformerEngine/main.cpp
+++ 2dPlatformerEngine/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <SDL.h>
+#include "Box2D/Box2D.h"
 
 // Only a test function
 void changeRenderer(SDL_Renderer *renderer)
@@ -13,7 +14,6 @@ void changeRenderer(SDL_Renderer *renderer)
 
 int main(int argc, char *argv[])
 {
-
 	SDL_Init(SDL_INIT_EVERYTHING);
 
 	// Flags to change window properties (Adding more flags requires | between each flag)
